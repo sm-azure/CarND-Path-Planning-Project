c3fe6027e98c30d3fc570f000c8621d1e58ef662
sm-azure <svanavi@DESKTOP-MJJ9189.localdomain>
2019-02-03 18:57:46 +0530

    drive in circles

--- src/main.cpp
+++ src/main.cpp
@@ -254,9 +254,36 @@ int main() {
 						cout << "Next Speed: " <<  next_speed << ", Distance POints:" << distance_betwee_points << endl;
 
 						
-						for(int i =0 ; i< 50; i++){
-							next_x_vals.push_back(car_x + distance_betwee_points* i * cos(deg2rad(car_yaw)));
-							next_y_vals.push_back(car_y + distance_betwee_points* i * sin(deg2rad(car_yaw)));
+						// for(int i =0 ; i< 50; i++){
+						// 	next_x_vals.push_back(car_x + distance_betwee_points* i * cos(deg2rad(car_yaw)));
+						// 	next_y_vals.push_back(car_y + distance_betwee_points* i * sin(deg2rad(car_yaw)));
+						// }
+						double pos_x;
+						double pos_y;
+						double angle;
+						int path_size = previous_path_x.size();
+						for (int i = 0; i < path_size; ++i) {
+							next_x_vals.push_back(previous_path_x[i]);
+							next_y_vals.push_back(previous_path_y[i]);
+						}
+						if (path_size == 0) {
+							pos_x = car_x;
+							pos_y = car_y;
+							angle = deg2rad(car_yaw);
+						} else {
+							pos_x = previous_path_x[path_size-1];
+							pos_y = previous_path_y[path_size-1];
+
+							double pos_x2 = previous_path_x[path_size-2];
+							double pos_y2 = previous_path_y[path_size-2];
+							angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
+						}
+
+						for (int i = 0; i < 50-path_size; ++i) {    
+							next_x_vals.push_back(pos_x+(distance_betwee_points)*cos(angle+(i+1)*(pi()/50)));
+							next_y_vals.push_back(pos_y+(distance_betwee_points)*sin(angle+(i+1)*(pi()/50)));
+							pos_x += (distance_betwee_points)*cos(angle+(i+1)*(pi()/50));
+							pos_y += (distance_betwee_points)*sin(angle+(i+1)*(pi()/50));
 						}
 
 
